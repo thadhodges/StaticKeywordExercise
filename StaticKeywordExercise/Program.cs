9f66f6907334cce79ab889a6c271d3d9c2aa0871
Thad <thadisfree@gmail.com>
2021-11-30 13:41:34 -0600

    first commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.FtoC(212);
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+            TempConverter.CtoF(100);
+        }
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeywordExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FtoC(double ftemp)
+        {
+            double newTemp =  (ftemp - 32) * 5 / 9;
+            
+            Console.WriteLine($"{ftemp} F is {newTemp} C");
+            return newTemp;
+        }
+
+        public static double CtoF(double ctemp)
+        {
+            double newTemp = ((ctemp / 5) * 9) + 32;
+            Console.WriteLine($"{ctemp} C is {newTemp} F");
+            return newTemp;
+        }
+
+    }
+}
